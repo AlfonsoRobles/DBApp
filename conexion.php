88881863654afb62b2fa6752378d63c178de5060
AlfonsoRobles <alfonso.robles8561@alumnos.udg.mx>
2026-07-08 20:43:00 -0600

    Refactor connection code in conexion.php

--- conexion.php
+++ conexion.php
@@ -1,15 +1,12 @@
 <?php
-// Datos de conexión a Railway
 $host = "hayabusa.proxy.rlwy.net";
 $user = "root";
-$password = "bDoZCfpoScHKONtCJHAXHvbcTMhbqgHp"; // reemplaza con la contraseña real
+$password = "bDoZCfpoScHKONtCJHAXHvbcTMhbqgHp"; 
 $database = "railway";
 $port = 42448;
 
-// Crear conexión
 $conexion = new mysqli($host, $user, $password, $database, $port);
 
-// Verificar conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
