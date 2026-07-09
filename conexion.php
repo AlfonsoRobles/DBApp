75dde017a718fd68ac549cb151a8201802fca2bd
AlfonsoRobles <alfonso.robles8561@alumnos.udg.mx>
2026-07-08 20:37:48 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,8 @@
 <?php
+// Datos de conexión a Railway
 $host = "hayabusa.proxy.rlwy.net";
 $user = "root";
-$password = "bDoZCfpoScHKONtCJHAXHvbcTMhbqgHp"; // reemplaza con la contraseña real que te da Railway
+$password = "bDoZCfpoScHKONtCJHAXHvbcTMhbqgHp"; // reemplaza con la contraseña real
 $database = "railway";
 $port = 42448;
 
@@ -12,6 +13,5 @@ $conexion = new mysqli($host, $user, $password, $database, $port);
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-
 
 
